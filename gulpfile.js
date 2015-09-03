dd621cd2291cb1cd5516f3e7ae3fd69a626602ea
Jitendra Vyas <jitendra.web@gmail.com>
2015-09-03 09:17:13 +0530

    my first edit

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync');
-var useref = require('gulp-useref'); 
+var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var minifyCSS = require('gulp-minify-css');
@@ -11,13 +11,18 @@ var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
 var del = require('del');
 var runSequence = require('run-sequence');
+var nunjucksRender = require('gulp-nunjucks-render');
+var data = require('gulp-data');
+var fs = require('fs`');
+var jsHint = require(gulp-jshint);
+var jscs = require('gulp-jscs');
 
 // Basic Gulp task syntax
-gulp.task('hello', function() {
-  console.log('Hello Zell!');
-})
+// gulp.task('hello', function() {
+//   console.log('Hello Zell!');
+// })
 
-// Development Tasks 
+// Development Tasks
 // -----------------
 
 // Start browserSync server
@@ -38,17 +43,26 @@ gulp.task('sass', function() {
     }));
 })
 
+gulp.task('watch-js', ['lint:js'], browserSync.reload);
+
 // Watchers
-gulp.task('watch', function() {
+gulp.task('watch', ['browserSync', 'sass'], function (){
   gulp.watch('app/scss/**/*.scss', ['sass']);
-  gulp.watch('app/*.html', browserSync.reload); 
-  gulp.watch('app/js/**/*.js', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', ['watch:js']);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+  gulp.watch([
+    'app/templates/**/*'
+    'app/pages/**/*.+(html|nunjucks)'
+    'app/data.json'
+    ], ['nunjucks'] // runs Nunjucks task
+  );
+
 })
 
-// Optimization Tasks 
+// Optimization Tasks
 // ------------------
 
-// Optimizing CSS and JavaScript 
+// Optimizing CSS and JavaScript
 gulp.task('useref', function() {
   var assets = useref.assets();
 
@@ -63,7 +77,7 @@ gulp.task('useref', function() {
     .pipe(gulp.dest('dist'))
 });
 
-// Optimizing Images 
+// Optimizing Images
 gulp.task('images', function() {
   return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
   // Caching images that ran through imagemin
@@ -73,13 +87,47 @@ gulp.task('images', function() {
   .pipe(gulp.dest('dist/images'))
 });
 
-// Copying fonts 
+// Compiling Nunjucks files to HTML
+gulp.task('nunjucks', function() {
+  nunjucksRender.nunjucks.configure(['app/templates/'], {watch: false});
+  return gulp.src('app/pages/**/*.+(html|nunjucks)')
+    .pipe(customPlumber('Error Running Nunjucks'))
+    .pipe(data(function() {
+      return JSON.parse(fs.readFileSync('./app/data.json'))
+    }))
+    .pipe(nunjucksRender())
+    .pipe(rename(function(path) {
+      path.extname = ".html"
+    }))
+    .pipe(gulp.dest('app'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+// Copying fonts
 gulp.task('fonts', function() {
   return gulp.src('app/fonts/**/*')
   .pipe(gulp.dest('dist/fonts'))
 })
 
-// Cleaning 
+gulp.task('lint:js', function() {
+  return gulp.src('app/js/**/*.js')
+  .pipe(customPlumber('JSHint Error'))
+  .pipe(jshint())
+  .pipe(jshint.reporter('jshint-stylish'))
+  .pipe(jshint.reporter('fail', {
+    ignoreWarning: true,
+    ignoreInfo: true
+  }))
+    .pipe(jscs({
+    fix: true,
+    configPath: '.jscsrc'
+  }))
+  .pipe(gulp.dest('app/js'))
+});
+
+// Cleaning
 gulp.task('clean', function(callback) {
   del('dist');
   return cache.clearAll(callback);
@@ -89,17 +137,24 @@ gulp.task('clean:dist', function(callback) {
   del(['dist/**/*', '!dist/images', '!dist/images/**/*'], callback)
 });
 
+
+
 // Build Sequences
 // ---------------
 
+// Consolidated dev phase task
 gulp.task('default', function(callback) {
-  runSequence(['sass', 'browserSync', 'watch'],
+  runSequence(
+    'clean:dev',
+    ['sprites', 'lint.js']
+    ['sass', 'nunjucks'],
+    ['browserSync', 'watch'],
     callback
   )
-})
+});
 
 gulp.task('build', function(callback) {
-  runSequence('clean:dist', 
+  runSequence('clean:dist',
     ['sass', 'useref', 'images', 'fonts'],
     callback
   )
